f6878692a23cda3b0923cf4e1fa8af7a44906245
ValentinaJakobson <jakobsonvv@gmail.com>
2019-10-09 21:36:59 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import useTodoState from './useTodoState';
 
-function App() {
+
+const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
   return (
     <div className="App">
-  
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+
+      <TodoForm
+        saveTodo={(todoText) => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }}
+      />
+
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
-}
+};
 
 export default App;
